3595c30c40bdef44d8b60e70fba3683cd7f07c88
xuanxuanhaha <zhangqixuan17@outlook.com>
2020-04-27 13:45:57 +1000

    finish until 8.1 learning and 8.2 add xadmin

--- MxOnline/settings.py
+++ MxOnline/settings.py
@@ -118,15 +118,15 @@ AUTH_USER_MODEL = "users.UserProfile"
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Australia/ACT'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin
+
+from apps.users.models import UserProfile
+
+# 对这个表创建管理器
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
+
+# 表的class，管理器的class
+admin.site.register(UserProfile, UserAdmin)
