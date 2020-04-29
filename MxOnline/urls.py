35e9b683da445f57e342aa5a27fbfbb30a713f2a
xuanxuanhaha <zhangqixuan17@outlook.com>
2020-04-29 22:00:35 +1000

    finish till learning 9.2, correct password -> home, wrong -> login

--- MxOnline/urls.py
+++ MxOnline/urls.py
@@ -19,9 +19,12 @@ from django.views.generic import TemplateView
 
 import xadmin
 
+from apps.users.views import LoginView
+
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('xadmin/', xadmin.site.urls),
-    path('', TemplateView.as_view(template_name="index.html")),
-    path('login/', TemplateView.as_view(template_name="login.html"), name="login")
+    path('', TemplateView.as_view(template_name="index.html"), name="index"),
+    path('login/', LoginView.as_view(), name="login")
 ]
+
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic.base import View
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+# 同时处理get和post请求
+class LoginView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, "login.html")
+    def post(self, request, *args, **kwargs):
+        user_name = request.POST.get("username", "")
+        password = request.POST.get("password", "")
+
+        # 用于通过用户和密码查询用户是否存在
+        user = authenticate(username=user_name, password=password)
+        if user is not None:
+            # 查询到用户
+            login(request, user)
+            # 登陆成功之后应该怎么返回页面
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            # 未查询到用户
+            return render(request, "login.html", {"msg":"用户名或密码错误"})
